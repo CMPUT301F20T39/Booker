db725a2141d72b49e689e99db28b85915d08e273
oluwashola omotosho <omotoshoshaun@gmail.com>
2020-10-29 17:08:01 -0700

    added state class & book status class. also changed the data type of status from string to enumeration

---
+++ app/src/main/java/com/example/booker/State.java
@@ -0,0 +1,28 @@
+package com.example.booker;
+
+import java.util.Objects;
+
+public class State {
+    private BookStatus bookStatus;
+
+    public State() {
+        this(BookStatus.AVAILABLE);
+    }
+
+    /**
+     * @param bookStatus: the book's status
+     */
+
+    public State(BookStatus bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+
+    public BookStatus getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(BookStatus bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+
+}
