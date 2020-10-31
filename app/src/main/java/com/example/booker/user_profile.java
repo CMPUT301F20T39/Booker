add20c83e282cbd0e1b0d3161e3a632ac6546083
Christian Arbelaez <arbelaezch@gmail.com>
2020-10-31 02:06:54 -0600

    Connected user_profile to BorrowerHome

--- app/src/main/java/com/example/booker/user_profile.java
+++ app/src/main/java/com/example/booker/user_profile.java
@@ -1,7 +1,11 @@
+package com.example.booker;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.booker.R;
 import com.google.firebase.firestore.DocumentReference;
@@ -19,7 +23,13 @@ public class user_profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        getSupportActionBar().setTitle("User Profile");
+
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        ActionBar myToolbar = getSupportActionBar();
+        myToolbar.setDisplayHomeAsUpEnabled(true);
+        myToolbar.setTitle("User Profile");
+
 
 
 
